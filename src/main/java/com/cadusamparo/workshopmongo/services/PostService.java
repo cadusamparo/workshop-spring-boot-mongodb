11ea72208cc368bb033746e25ef639acb67aad26
Carlos Eduardo Braz Samparo <carlossamparoo@gmail.com>
2023-05-04 10:40:36 -0300

    Obtendo um post por id

---
+++ src/main/java/com/cadusamparo/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.cadusamparo.workshopmongo.services;
+
+import com.cadusamparo.workshopmongo.domain.Post;
+import com.cadusamparo.workshopmongo.domain.User;
+import com.cadusamparo.workshopmongo.repository.PostRepository;
+import com.cadusamparo.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
