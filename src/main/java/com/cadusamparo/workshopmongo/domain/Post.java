d9edd323cf2dfe7c6db05c18676a6cf1cb42033d
Carlos Eduardo Braz Samparo <carlossamparoo@gmail.com>
2023-05-04 10:52:23 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/cadusamparo/workshopmongo/domain/Post.java
+++ src/main/java/com/cadusamparo/workshopmongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.cadusamparo.workshopmongo.domain;
 
 import com.cadusamparo.workshopmongo.dto.AuthorDTO;
+import com.cadusamparo.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -29,6 +32,7 @@ public class Post implements Serializable {
         this.body = body;
         this.author = author;
     }
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public String getId() {
         return id;
@@ -68,6 +72,14 @@ public class Post implements Serializable {
 
     public AuthorDTO getAuthor() {
         return author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
